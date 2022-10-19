e96fcbfb66f5d71ca6f253cc3674faa81e4e815e
Zakharosomaha <khakim.zakhriddin@mail.com>
2022-10-19 14:07:30 +0300

    dz-js3

---
+++ main.js
@@ -0,0 +1,53 @@
+// function compare(number1, number2){
+// if(number1>number2){
+//     alert(number1 + '  больше, чем  ' + number2)
+// }
+// else if(number1<number2){
+//     alert(number1 + '  меньше, чем ' + number2)
+// }
+// else{
+//     (number1=number2)
+//         alert(number1 + ' равно ' + number2)
+//     }
+// }
+// const number1 = prompt('Введите первое число')
+// const number2 = prompt('Введите второе число')
+
+// compare(number1, number2)
+
+
+
+
+
+
+
+// function twoMass (array1, array2){
+//     if (array1>array2){
+//         alert (array1 + ' первый массив длиннее второго =' + array2)
+//     } else if(array1<array2){
+//         alert (array1 + ' первый массив короче второго ' + array2)
+//     }else{
+//         (array1=array1)
+//         alert('массивы равны между собой ')   
+//     }
+// }
+// const array1 = prompt('Введите данные ')
+// const array2 = prompt('Введите данные для сравнения')
+
+// twoMass(array1.length, array2.length)
+
+
+
+
+// function countChar(argCount){
+//     let value = 0;
+//     for (let i = 1; i <= argCount; i++) {
+//         value = i;   
+//     }
+//     console.log(value);
+// }
+// const arg = prompt('Введите значение').replaceAll(' ', '');
+
+// countChar(arg.length)
+
+
